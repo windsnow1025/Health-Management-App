91dbb25678472288683dd5d733fa802f4c89785d
Admiraljj <89696583@qq.com>
2023-10-07 23:10:15 +0800

    modify the ExampleInstrumentedTest.java add //test

--- app/src/androidTest/java/com/project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/project/ExampleInstrumentedTest.java
@@ -15,6 +15,9 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
+//test
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
