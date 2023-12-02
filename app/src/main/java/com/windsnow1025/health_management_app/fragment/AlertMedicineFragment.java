b69794734aace496a2a17551552e1e7d76ed216c
windsnow1025 <windsnow1025@gmail.com>
2023-12-02 13:58:27 +0000

    Update AlertMedicineFragment.java

--- app/src/main/java/com/windsnow1025/health_management_app/fragment/AlertMedicineFragment.java
+++ app/src/main/java/com/windsnow1025/health_management_app/fragment/AlertMedicineFragment.java
@@ -133,6 +133,8 @@ public class AlertMedicineFragment extends Fragment {
 
         /*表修改状态，非新增时*/
         if (flag) {
+//            Alert alert1 = alertArrayList.get(alert_id);
+            // Idk why i can't use the above line, so i use the following line instead.
             Alert alert1 = userLocalDao.getAlert(alertArrayList, alert_id);
             et_title.setText(alert1.getTitle());
             et_time.setText(alert1.getAlert_date());
